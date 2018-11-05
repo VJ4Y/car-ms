3ca5bd1f673fd83820c69c2a32f29480d239e7ca
VJ@Y <vjrnjn@gmail.com>
2018-11-05 23:38:27 +0530

    db props

--- src/main/java/com/yondu/carms/CarMsApplication.java
+++ src/main/java/com/yondu/carms/CarMsApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.yondu.carms.domain.Car;
 import com.yondu.carms.domain.Owner;

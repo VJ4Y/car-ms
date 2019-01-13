adee86f3c793d9dab6680e9e5a34ecc01826759a
VJ@Y <vjrnjn@gmail.com>
2019-01-13 20:57:08 +0530

    security config change

--- src/main/java/com/yondu/carms/SecurityConfig.java
+++ src/main/java/com/yondu/carms/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.yondu.carms;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,12 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.cors().and().authorizeRequests().anyRequest().permitAll();
+	}
 	
 	@Bean
 	@Override

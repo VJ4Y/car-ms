347dca90f06e3531f0f6d2dfc36164d2c67c2baf
VJ@Y <vjrnjn@gmail.com>
2019-01-13 20:32:57 +0530

    adding cors origin annotaion to cars repository

--- src/main/java/com/yondu/carms/repository/CarRepository.java
+++ src/main/java/com/yondu/carms/repository/CarRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.yondu.carms.domain.Car;
 
+@CrossOrigin
 @Repository
 public interface CarRepository extends CrudRepository<Car, Integer> {
 
